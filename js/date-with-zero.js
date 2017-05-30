002148a1e8d9b1fbdd5c4cc0df7d02fdbf4ad7b1
DaveEveritt <deveritt@innotts.co.uk>
2017-05-30 21:30:18 +0100

    Initial commit

---
+++ js/date-with-zero.js
@@ -0,0 +1,23 @@
+(function(){
+
+  const d = new Date();
+  
+  zeroPrefix = function (needZero) {
+    return (needZero < 10) ? `0${needZero}` : needZero;
+  };
+  
+  const now = {
+    day: function() {
+      const day = d.getDate();
+      return zeroPrefix(day);
+    },
+    month: function() {
+      const month = d.getMonth()+1;
+      return zeroPrefix(month);
+    },
+    year: d.getFullYear()
+  }
+
+  document.getElementById("date").innerHTML=`${now.day()}/${now.month()}/${now.year}`;
+
+})();
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Today's date with leading zeros
+
+A simple script that adds a leading '0' to any day/month less than '10'.
+
+For example, 2nd May 2017 would simply be '2/5/2017', so this makes it '02/05/2017'.
+
+Uses? For lining up columns of dates nicely. Or anything else you can think of.
+
+## TODO:
+
+Rewrite so you can use this to add leading zeros to any date passed in as a parameter.
